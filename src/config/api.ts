1c9153a3ed4ce955ddabe056b47ee32ad50538fa
sergiivelikorodnov <sergii.velikorodnov@gmail.com>
2022-06-03 14:03:03 +0200

    fix: prettier

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,14 +1,15 @@
-export const API_KEY = "m409XzLNTZAmVYXLmz7SkmUf3XCvGf9N"
-export const API_DOMAIN = "https://api.apilayer.com/exchangerates_data/convert?"
+export const API_KEY = "m409XzLNTZAmVYXLmz7SkmUf3XCvGf9N";
+export const API_DOMAIN =
+  "https://api.apilayer.com/exchangerates_data/convert?";
 
 const myHeaders = new Headers();
 myHeaders.append("apikey", API_KEY);
 
 export const requestOptions = {
-  method: 'GET',
-  redirect: 'follow' as RequestRedirect,
-  headers: myHeaders
+  method: "GET",
+  redirect: "follow" as RequestRedirect,
+  headers: myHeaders,
 };
 
-export const endpointPath = (from:string, to:string, amount:string) =>
-   `${API_DOMAIN}to=${to}&from=${from}&amount=${amount}`
+export const endpointPath = (from: string, to: string, amount: string) =>
+  `${API_DOMAIN}to=${to}&from=${from}&amount=${amount}`;
